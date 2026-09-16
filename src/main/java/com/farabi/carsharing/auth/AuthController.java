dec12e22a721ee372d3940c97728db160abca4cb
F4rab1 <issafarabi2022@gmail.com>
2026-09-16 23:36:52 +0500

    Accessing the Current User

--- src/main/java/com/farabi/carsharing/auth/AuthController.java
+++ src/main/java/com/farabi/carsharing/auth/AuthController.java
@@ -1,16 +1,22 @@
 package com.farabi.carsharing.auth;
 
+import com.farabi.carsharing.users.UserDto;
+import com.farabi.carsharing.users.UserMapper;
+import com.farabi.carsharing.users.UserRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
 
@@ -35,5 +41,20 @@ public class AuthController {
         var token = authHeader.replace("Bearer ", "");
 
         return jwtService.validateToken(token);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var email = (String) authentication.getPrincipal();
+
+        var user = userRepository.findByEmail(email).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var userDto = userMapper.toDto(user);
+
+        return ResponseEntity.ok(userDto);
     }
 }

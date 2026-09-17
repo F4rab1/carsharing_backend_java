8ddb066c4fcd690e333a844f8782f1c8813303b1
F4rab1 <issafarabi2022@gmail.com>
2026-09-17 23:04:50 +0500

    Enhancing the JWT Payload

--- src/main/java/com/farabi/carsharing/auth/AuthController.java
+++ src/main/java/com/farabi/carsharing/auth/AuthController.java
@@ -30,7 +30,9 @@ public class AuthController {
                 )
         );
 
-        var token = jwtService.generatetoken(request.getEmail());
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+
+        var token = jwtService.generatetoken(user);
 
         return ResponseEntity.ok(new JwtResponse(token));
     }
@@ -46,15 +48,15 @@ public class AuthController {
     @GetMapping("/me")
     public ResponseEntity<UserDto> me() {
         var authentication = SecurityContextHolder.getContext().getAuthentication();
-        var email = (String) authentication.getPrincipal();
+        var userId = (Long) authentication.getPrincipal();
 
-        var user = userRepository.findByEmail(email).orElse(null);
+        var user = userRepository.findById(userId).orElse(null);
         if (user == null) {
             return ResponseEntity.notFound().build();
         }
 
-        var userDto = userMapper.toDto(user);
+        var userResponseDto = userMapper.toDto(user);
 
-        return ResponseEntity.ok(userDto);
+        return ResponseEntity.ok(userResponseDto);
     }
 }
--- src/main/java/com/farabi/carsharing/auth/JwtService.java
+++ src/main/java/com/farabi/carsharing/auth/JwtService.java
@@ -1,5 +1,6 @@
 package com.farabi.carsharing.auth;
 
+import com.farabi.carsharing.users.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
@@ -14,11 +15,13 @@ public class JwtService {
     @Value("${spring.jwt.secret}")
     private String secretKey;
 
-    public String generatetoken(String email) {
+    public String generatetoken(User user) {
         final long tokenExpiration = 86400; // 1 day in seconds
 
         return Jwts.builder()
-                .subject(email)
+                .subject(user.getId().toString())
+                .claim("email", user.getEmail())
+                .claim("name", user.getName())
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
                 .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
@@ -34,6 +37,10 @@ public class JwtService {
         catch (JwtException ex) {
             return false;
         }
+    }
+
+    public Long getUserIdFromToken(String token) {
+        return Long.valueOf(getClaims(token).getSubject());
     }
 
     public String getEmailFromToken(String token) {
